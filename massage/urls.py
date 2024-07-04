fe0509362f1de986153eb447e2ef9d5544995efe
legend <weiche2142@gmail.com>
2024-07-04 16:22:08 +0800

    查看新增留言、登入

--- massage/urls.py
+++ massage/urls.py
@@ -4,5 +4,10 @@ from .import views
 urlpatterns = [
     # massage/     =>  留言列表
     path('', views.MassageList.as_view(), name='msg_list'),
+    #massage/1/, massage/2/, ...
+    path('<int:pk>/', views.MassageRead.as_view(), name='msg_read'),
+
+    path('', views.MassageNew.as_view(), name='msg_new')
+
    
 ]
--- massage/views.py
+++ massage/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Massage
 
 # Create your views here.
 class MassageList(ListView):
     model = Massage
 
-    
+    # 頁面範本檔案名稱:應用程式/資料模型_list.html => massage/massage_list.html
+
+class MassageRead(DetailView):
+    model = Massage
+
+    #頁面範本檔案名稱:應用程式/資料模型_detail.html => massege/massage_detail......
+    #頁面範本變數名稱:資料模型=> massage
+
+class MassageNew(CreateView):
+    model = Massage
+    fields = ['user', 'receipt', 'subject', 'content'] 
+    success_url='/message/' 
+
+    #fields = '__all__'    ->顯示全部欄位
+
+    #頁面範本檔案名稱:應用程式/資料模型_form.html => massege/massage_detail......
+    #頁面範本變數名稱:資料模型=> massage
