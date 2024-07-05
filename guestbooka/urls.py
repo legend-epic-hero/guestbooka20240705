5e0f41ab9c77afe79f6a43290f94758a6d064dba
legend <weiche2142@gmail.com>
2024-07-05 14:56:43 +0800

    限制刪除留言(只能登入後才能操作

--- guestbooka/urls.py
+++ guestbooka/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# =>以上是python的多行註解
+
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('massage/', include('massage.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
--- massage/views.py
+++ massage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import Massage
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class MassageList(ListView):
@@ -26,7 +27,7 @@ class MassageNew(CreateView):
     #頁面範本檔案名稱:應用程式/資料模型_form.html => massege/massage_detail......
     #頁面範本變數名稱:資料模型=> massage
 
-class MassageDelete(DeleteView):
+class MassageDelete(LoginRequiredMixin,DeleteView):
     model = Massage
-    success_url = '/massage/'
+    success_url = reverse_lazy('msg_list')
     # 頁面範本檔案名稱:應用程式/資料模型_confirm_delete.html => massage/Massage _confirm_delete.html
